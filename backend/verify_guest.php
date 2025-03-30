3bc0deffa15760a24b81619c017942d30104cf72
Cole Wales <mavie@kali.lan>
2025-03-29 19:27:15 -0500

    Commenting out error logging

--- backend/verify_guest.php
+++ backend/verify_guest.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'config.php'; // Database connection & session
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL);
 
 header('Content-Type: application/json');
 
